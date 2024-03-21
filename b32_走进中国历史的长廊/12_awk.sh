20bcbbb53e2a5fca425b6125996722d46bd5cc88
mymy20220326 <mymy20220326@outlook.com>
2024-03-21 20:15:38 +0800

    2024_03_21_0210-wmtag_memo_调整一下样例的内容

--- b32_走进中国历史的长廊/12_awk.sh
+++ b32_走进中国历史的长廊/12_awk.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# grasspy.cn
+# 草蟒极速 QQ 交流群：760167264 风里雨里我们在这个QQ群里等你 快来吧,大家一起快乐的聊天吧!
+# 来聊聊,中西如何互为借鉴,砥砺前行,中西合璧,走进新时代,实践全球化的命运共同体
 
+# NOTE 多种_运行_方法_下面的每种方法_都是独立进行的
+# 02_在本编辑器中_点击鼠标右键_选择_"Run Code"的_菜单选项
+# 03_在本编辑器中_按组合键_Alt+Ctrl+N
 
 /.PlnPyKFp4CRfFtgC1/vendor/modules/code-oss-dev/bin/remote-cli/cloudstudio \
     d33_邱小姐在梳妆台梳辫子_熔盐热能电厂.hdnb
